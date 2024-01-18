ba00bdaac643b8be649fd69f32641d53d2d50945
Yulissa <lyulissa5@gmail.com>
2024-01-17 19:03:06 -0500

    solving error

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import Form from './components/form';
 import  Results  from './components/Results';
+import Card from './components/Card';
+import SearchBar from './components/SearchBar';
 
 export default function App() {
   return (
-    <>
-      <Form />
-      <Results />
-    </>
+    <div className='flex flex-col items-center '>
+      <SearchBar />
+      <Card />
+    </div>
   );
 }
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,9 @@
+export default function SearchBar () {
+  return (
+    <>
+
+    <input type="text" className="border-2 border-gray-100 white h-14 w-1/3 px-4 rounded-full focus:outline-none hover:cursor-pointer" placeholder= "Algoritmo, variable, arreglo" name="search" />
+</>
+
+  );
+}
