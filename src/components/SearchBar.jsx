f79a5085c3fff237816bbffd29f72da845bf064c
Yulissa <lyulissa5@gmail.com>
2024-01-17 19:18:44 -0500

    change branch cardComponent to uiComponent

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,7 +3,7 @@ export default function SearchBar () {
     <>
 
     <input type="text" className="border-2 border-gray-100 white h-14 w-1/3 px-4 rounded-full focus:outline-none hover:cursor-pointer" placeholder= "Algoritmo, variable, arreglo" name="search" />
-</>
+    </>
 
   );
 }
